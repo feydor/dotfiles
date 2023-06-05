26ade9f10e6cbb847ab29786c0b8f8903982332e
feydor <victorreydev@outlook.com>
2023-06-05 00:03:58 -0500

    Add a README & install scripts

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -11,17 +11,3 @@
  '(org-agenda-files '("~/Documents/1.org"))
  '(package-selected-packages
    '(hl-todo autothemer app-launcher openwith use-package markdown-mode rainbow-delimiters paredit slime cmake-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka" :height 1.6 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka" :height 1.5))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka" :height 1.3))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka" :height 1.2))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#000000" :font "Iosevka")))))
